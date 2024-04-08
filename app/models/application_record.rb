3f7ee2c3e112866cee73a119e22cade8529112fd
Bhupendra Singh <aventumcormac@hotmail.com>
2024-04-08 14:38:14 +0530

    initialize paranoia for soft delete.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  primary_abstract_class # default in rails 7
+
+  self.abstract_class = true
+
+  acts_as_paranoid
 end

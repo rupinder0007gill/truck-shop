bdf1b0f314c6b7d8b77654627f9d468723e3739a
Rupinder Gill <rupinder0007gill@gmail.com>
2024-04-11 20:50:00 +0530

    CRUD for users

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::UsersController < ApplicationController
+  before_action :set_user, only: [ :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -11,12 +13,28 @@ class Users::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    if @user.save
-      redirect_to users_user_path, notice: 'User was successfully created.'
+    if @user.save!
+      redirect_to users_user_path(@user), notice: 'User was successfully created.'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save!
+      redirect_to users_user_path(@user), notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_user_path, notice: 'User was successfully deleted.'
   end
 
   private
@@ -26,6 +44,6 @@ class Users::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :phone, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :role_id)
   end
 end

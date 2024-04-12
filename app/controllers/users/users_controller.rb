bad37dc5050cda04dddd6d853b9624d3b60ca9f9
Rupinder Gill <rupinder0007gill@gmail.com>
2024-04-12 10:55:41 +0530

    user page theme design and added phone number column

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -44,6 +44,6 @@ class Users::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :role_id)
+    params.require(:user).permit(:first_name, :last_name, :email, :phone, :password, :role_id)
   end
 end
